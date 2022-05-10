9c551967d3036b1731d47d3622aaef3d7e0455ac
sergio2pj <sergio6pj@gmail.com>
2022-05-10 15:34:09 -0300

    Add. style-components e refatorando código do arquivos Cards

--- my-app/src/components/Card/index.js
+++ my-app/src/components/Card/index.js
@@ -1,26 +1,35 @@
-import React from "react";
-export default function Card({ card }) {
+import styled from 'styled-components'
+const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 20%;
+    height: 20%;
+    background-color: ${(props) =>
+        props.status === "Dead" ? "red" : props.status === "unknown" ? "gray" : ""};
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+    margin: 10px;
+    padding: 10px;
+    h2 {
+        font-size: 1.5rem;
+        font-weight: bold;
+    }
+    p {
+        font-size: 1rem;
+    }
+`;
+function CardComponent({ card }) {
     return (
-        React.createElement(
-            "div",
-            { className: `card_${card.status}` },
-            React.createElement("img", {
-                src: card.image,
-                alt: card.name,
-                key: card.id,
-            }),
-            React.createElement("h2", null, card.name),
-            React.createElement("p", null, `Status: ${card.status}`),
-            React.createElement("p", null, `Species: ${card.species}`),
-            React.createElement("p", null, `Type: ${card.type}`),
-            React.createElement(
-                "p",
-                null,
-                `Origin: ${card.origin.name}`,
-                React.createElement("br", null),
-                `Location: ${card.location.name}`
-            )
-            
-        )
-    );
+        <Card status={card.status}>
+            <h2>{card.name}</h2>
+            <img src={card.image} alt={card.name} />
+            <p>{card.species}</p>
+            <p>{card.type}</p>
+            <p>{card.location.name}</p>
+            <p>{card.origin.name}</p>
+        </Card>
+    )
 }
+export default CardComponent;
